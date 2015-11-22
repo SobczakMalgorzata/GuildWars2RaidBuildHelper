790193d3d0bbd44475198970d083abb2bf565815
Sobczak Malgorzata <sobczakmalgorz@gmail.com>
2015-11-22 21:44:54 +0100

    Starting Project
    Initial Commit

---
+++ GuildWars2RaidBuildHelper/FileSaver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildWars2RaidBuildHelper
+{
+    class FileSaver
+    {
+        void BuildSaver()
+        {
+
+        }
+
+        void SquadBuildSaver()
+        {
+
+        }
+    }
+}
